b867085347e415c2df3dee57a37b4f779751bcc2
JC Bombardelli <jc.bombardelli@live.com>
2017-06-17 18:02:14 -0300

    Classes de Dominio adicionadas
    Feeder, identifica um ponto de venda que cadastrará leads
    Lead, identifica uma pessoa cadastrada por um Feeder

---
+++ SportMe.One/SportMe.One/Domain/Feeder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportMe.One.Domain
+{
+    public class Feeder
+    {
+
+      
+        public string User { get; set; }
+        public string Pass { get; set; }
+        public string Club { get; set; }
+
+
+        public Feeder(string user, string pass)
+        {
+            User = user;
+            Pass = pass;
+        }
+    }
+}
---
+++ SportMe.One/SportMe.One/Domain/Lead.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportMe.One.Domain
+{
+    public class Lead
+    {
+
+        [JsonIgnore]
+        public int ID { get; set; }
+
+        public string nome { get; set; } = string.Empty;
+        public string email { get; set; } = string.Empty;
+        public string cpf { get; set; } = string.Empty;
+        public string valor { get; set; } = string.Empty;
+        public string setor { get; set; } = string.Empty;
+        public string idade { get; set; } = string.Empty;
+        public string datahora { get; set; }
+
+        [JsonIgnore]
+        public bool Sync { get; set; } = false;
+    }
+}
