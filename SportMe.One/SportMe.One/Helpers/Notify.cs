21ea1b77c2631510c41b514212ed867beb4dea77
JC Bombardelli <jc.bombardelli@live.com>
2017-06-17 18:03:38 -0300

    Helpers Guardian e Notify adicionados
    Guardian utlizado para validar entrada de dados
    Notify para atualizar camadas acima de alteração

---
+++ SportMe.One/SportMe.One/Helpers/Notify.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace SportMe.One.Helpers
+{
+    public class Notify : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public static event PropertyChangedEventHandler StaticPropertyChanged;
+
+
+        public static void OnPropertyChanged(object obj, string propertyName = null)
+        {
+            StaticPropertyChanged?.Invoke(obj, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
