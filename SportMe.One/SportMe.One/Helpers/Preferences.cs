791cc16eb1d1ee8b90d42ee4c0e7788848253f21
JC Bombardelli <jc.bombardelli@live.com>
2017-06-17 18:00:29 -0300

    Gerenciamento de Preferencias adicionado

---
+++ SportMe.One/SportMe.One/Helpers/Preferences.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using SportMe.One.IOC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SportMe.One.Helpers
+{
+    public class Preferences<E>
+    {
+
+        private const string FILE_NAME = "preferences.json";
+
+        public object GetPreferences()
+        {
+            var fileLoadedInString = DependencyService.Get<IDeviceStorage>().ReadAsString(FILE_NAME);
+            return JsonConvert.DeserializeObject<E>(fileLoadedInString);
+        }
+
+        public object SetPreferences(E preferences)
+        {
+            DependencyService.Get<IDeviceStorage>().SaveAsString(FILE_NAME, JsonConvert.SerializeObject(preferences));
+            return GetPreferences();
+
+        }
+
+    }
+}
---
+++ SportMe.One/SportMe.One/Helpers/PreferencesOne.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportMe.One.Helpers
+{
+    public class PreferencesOne
+    {
+
+        private DateTime primeiroLogin;
+        private DateTime fechamentoApp;
+        private string usuario;
+        private bool lembrarMe = false;
+        private string clube;
+
+
+        public bool LembrarMe
+        {
+            get { return lembrarMe; }
+            set { lembrarMe = value; }
+        }
+
+        public string Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+
+        public string Clube
+        {
+            get { return clube; }
+            set { clube = value; }
+        }
+
+        public DateTime PrimeiroLogin
+        {
+            get { return primeiroLogin; }
+            set { primeiroLogin = value; }
+        }
+
+
+
+
+        private static PreferencesOne instance = (PreferencesOne)new Preferences<PreferencesOne>().GetPreferences();
+
+        private PreferencesOne()
+        { }
+
+        public static PreferencesOne Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = (PreferencesOne)new Preferences<PreferencesOne>().SetPreferences(new PreferencesOne());
+
+                return instance;
+            }
+        }
+
+        public void Save()
+        {
+            new Preferences<PreferencesOne>().SetPreferences(this);
+        }
+
+        public void SaveApp()
+        {
+            fechamentoApp = DateTime.UtcNow;
+            Save();
+
+        }
+
+        public void Reset()
+        {
+            instance = (PreferencesOne)new Preferences<PreferencesOne>().SetPreferences(new PreferencesOne());
+        }
+
+
+    }
+}
