9b2241943dcd879f172da53a75ae8eaf5059cd7a
JC Bombardelli <jc.bombardelli@live.com>
2017-06-17 17:57:52 -0300

    Implementação para DeviceStorage adicionado
    Utilizado para gerenciar arquivos dentro da aplicação

---
+++ SportMe.One/SportMe.One.Android/IOC/DeviceStorage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using SportMe.One.IOC;
+
+
+
+[assembly: Dependency(typeof(SportMe.One.Droid.IOC.DeviceStorage))]
+namespace SportMe.One.Droid.IOC
+{
+    public class DeviceStorage : IDeviceStorage
+    {
+
+        private string documentPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+
+
+        public string ReadAsString(string filename)
+        {
+            var filePath = Path.Combine(documentPath, filename);
+
+            if (!File.Exists(filePath))
+                SaveAsString(filePath, string.Empty);
+
+            return File.ReadAllText(filePath);
+        }
+
+        public void SaveAsString(string filename, string content)
+        {
+
+            var filePath = Path.Combine(documentPath, filename);
+            File.WriteAllText(filePath, content);
+        }
+    }
+}
---
+++ SportMe.One/SportMe.One/IOC/IDeviceStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportMe.One.IOC
+{
+    public interface IDeviceStorage
+    {
+
+        string ReadAsString(string filename);
+
+        void SaveAsString(string filename, string content);
+
+    }
+}
