a34d8b35adf3ff3fcfb1509e6b5f9a48eedbc126
JC Bombardelli <jc.bombardelli@live.com>
2017-06-17 18:05:02 -0300

    BaseViewModel
    Classe para gerenciamento de todas as ViewModel's

---
+++ SportMe.One/SportMe.One/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using SportMe.One.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportMe.One.ViewModel.Base
+{
+    public class BaseViewModel
+    {
+
+        bool _bIsLoading = false;
+        public bool IsLoading
+        {
+            get { return _bIsLoading; }
+            set
+            {
+                if (_bIsLoading != value)
+                {
+                    _bIsLoading = value;
+                    Notify.OnPropertyChanged(this, nameof(IsLoading));
+                }
+            }
+        }
+    }
+}
