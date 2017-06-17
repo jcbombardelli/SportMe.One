dedf115ccb8986de2834554be7091a38921e4e02
JC Bombardelli <jc.bombardelli@live.com>
2017-06-17 18:08:51 -0300

    LeadEditView adicionado
    Tela para cadastro de um Lead por um Feeder

---
+++ SportMe.One/SportMe.One/View/Lead/LeadEditView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SportMe.One.View
+{
+
+    public partial class LeadEditView : ContentPage
+    {
+        public LeadEditView()
+        {
+            InitializeComponent();
+        }
+    }
+}
