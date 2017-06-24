8a8461df0271907c443fb07eaba66e1ab7aac940
JC Bombardelli <jc.bombardelli@live.com>
2017-06-24 14:31:45 -0300

    LeadEdit Updated v1.1

--- SportMe.One/SportMe.One/View/Lead/LeadEditView.xaml.cs
+++ SportMe.One/SportMe.One/View/Lead/LeadEditView.xaml.cs
@@ -1,20 +1,128 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using SportMe.One.Domain;
+using SportMe.One.Helpers;
+using SportMe.One.ViewModel;
+using System;
+using System.Globalization;
+using System.Net;
+using System.Text.RegularExpressions;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace SportMe.One.View
 {
 
     public partial class LeadEditView : ContentPage
     {
+
+        private LeadEditViewModel leadEditViewModel = new LeadEditViewModel();
+        private RestClient rc = new RestClient();
+
+
+
         public LeadEditView()
         {
             InitializeComponent();
+            BindingContext = leadEditViewModel;
+        }
+
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+
+            var url = string.Format("{0}{1}?club={2}", App.SportMeURL, "clubs/sectors", "Santos");
+
+            var result = await rc.Request(url, RestClient.HTTPMethod.GET);
+
+            string verfi = await result.Content.ReadAsStringAsync();
+            verfi = verfi.Replace('\"', ' ');
+            verfi = verfi.Replace('[', ' ');
+            verfi = verfi.Replace(']', ' ');
+            verfi = verfi.Trim();
+            var p = verfi.Split(',');
+
+            pickerLead.ItemsSource = p;
+
+
+
+
+        }
+
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+
+            leadEditViewModel.Lead.Datahora = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss", CultureInfo.CurrentUICulture);
+
+
+            if (App.CheckInternetConnection())
+            {
+                try
+                {
+
+                    var result = await rc.Request("https://sportsmeleads.herokuapp.com/api/leads", RestClient.HTTPMethod.POST, leadEditViewModel.Lead);
+
+                    if (result.StatusCode == HttpStatusCode.OK || result.StatusCode == HttpStatusCode.Accepted)
+                    {
+                        leadEditViewModel.Lead.Sync = true;
+                        await DisplayAlert("SportMe Leads", "Lead Adicionado e Sincronizado", "OK");
+                    }
+                    else
+                    {
+                        leadEditViewModel.Lead.Sync = false;
+                        await DisplayAlert("SportMe Leads", "Lead Adicionado, mas não sincronizado, erro: " + result.StatusCode, "OK");
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    await DisplayAlert("SportMe Leads", "Lead Adicionado sem sincronização.", "OK");
+                }
+            }
+
+            leadEditViewModel.Lead = new Lead();
+        }
+
+        private async void Entry_Unfocused(object sender, FocusEventArgs e)
+        {
+
+            Entry entry = sender as Entry;
+
+
+            if (!string.IsNullOrEmpty(entry.Text))
+            {
+
+                entry.Unfocused -= Entry_Unfocused;
+
+                if (!Regex.IsMatch(entry.Text, "^((\\d*)[0-9])+[.][0-9]{2}"))
+                {
+                    await DisplayAlert("Formato Incorreto", "Verifique o valor do ingresso", "Vou verificar!");
+                }
+
+                entry.Unfocused += Entry_Unfocused;
+
+            }
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (e.Value)
+                txtSex.Text = "Feminino";
+            else
+                txtSex.Text = "Masculino";
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var split = e.NewTextValue.Split('.');
+
+            if (split.Length > 1 && split[1].Length > 2)
+            {
+                Entry entry = sender as Entry;
+                entry.Text = e.NewTextValue.Remove(e.NewTextValue.Length - 1);
+            }
         }
     }
 }
---
+++ SportMe.One/SportMe.One/ViewModel/Lead/LeadEditViewModel.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using SportMe.One.Domain;
+using SportMe.One.Helpers;
+using SportMe.One.View;
+using SportMe.One.ViewModel.Base;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SportMe.One.ViewModel
+{
+    public class LeadEditViewModel : BaseViewModel
+    {
+
+
+        #region Getters and Setters
+        private Lead lead;
+        public Lead Lead
+        {
+            get { return lead; }
+            set
+            {
+                lead = value;
+                OnPropertyChanged(nameof(Lead));
+            }
+        }
+
+        #endregion
+
+
+
+        public ICommand RegisterCommand { get; set; }
+
+
+        public LeadEditViewModel() : base()
+        {
+            RegisterCommand = new Command(DoRegister);
+            lead = new Lead();
+        }
+
+
+
+        private async void DoRegister()
+        {
+            lead.Datahora = DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss", CultureInfo.CurrentUICulture);
+
+
+            if (App.CheckInternetConnection())
+            {
+                try
+                {
+                    var rc = new RestClient();
+                    var result = await rc.Request("https://sportsmeleads.herokuapp.com/api/leads", RestClient.HTTPMethod.POST, Lead);
+
+                    if (result.StatusCode == HttpStatusCode.OK || result.StatusCode == HttpStatusCode.Accepted)
+                    {
+                        Lead.Sync = true;
+                        //await DisplayAlert("SportMe Leads", "Lead Adicionado e Sincronizado", "OK");
+                    }
+                    else
+                    {
+                        Lead.Sync = false;
+                        //await DisplayAlert("SportMe Leads", "Lead Adicionado, mas não sincronizado, erro: " + result.StatusCode, "OK");
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    // await DisplayAlert("SportMe Leads", "Lead Adicionado sem sincronização.", "OK");
+                }
+            }
+
+            Lead = new Lead();
+        }
+
+    }
+}
