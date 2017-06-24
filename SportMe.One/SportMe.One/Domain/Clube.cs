a258c69ad2db601d08545c58c2178d0ee996abae
JC Bombardelli <jc.bombardelli@live.com>
2017-06-24 14:29:54 -0300

    Domain Clube added

---
+++ SportMe.One/SportMe.One/Domain/Clube.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportMe.One.Domain
+{
+    public class Clube
+    {
+
+        public string Nome { get; set; }
+        public string[] Setores { get; set; }
+
+
+
+        public Clube()
+        {
+
+        }
+
+
+        public Clube(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
